b8ed983bded96ad2c27a7d6af80967bfad43846b
imsamnang <applephagna@gmail.com>
2019-05-09 23:17:12 +0700

    update db:seed
    update db:seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,13 +4,10 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $this->call(CommunesTableSeeder::class);
-    }
+
+  public function run()
+  {
+      $this->call(CommunesTableSeeder::class);
+      $this->call(PhoneOperatorSeeder::class);
+  }
 }
